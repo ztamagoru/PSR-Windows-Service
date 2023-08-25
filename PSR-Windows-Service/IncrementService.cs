836db6b193fb8671da1a21688dd5c6cc2e451d54
tamagoru <marushannnn@outlook.com>
2023-08-25 11:51:15 -0300

    Add project files.

---
+++ PSR-Windows-Service/IncrementService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Timers;
+using System.IO;
+using System.ServiceProcess;
+
+namespace IncrementService
+{
+    public class IncrementService : ServiceBase
+    {
+        private Timer timer;
+        private int counter;
+
+        public IncrementService()
+        {
+            this.ServiceName = "IncrementService";
+            this.CanStop = true;
+            this.CanPauseAndContinue = true;
+            this.AutoLog = true;
+        }
+
+        protected override void OnStop()
+        {
+            timer.Stop();
+        }
+
+        private void OnTimer(object sender, ElapsedEventArgs e)
+        {
+            counter++;
+
+            string path = System.Reflection.Assembly.GetExecutingAssembly()
+               .Location + @"\..\..\..\..\PSR-Windows-Service\Resources\counter.txt";
+
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(counter);
+            }
+        }
+    }
+}
